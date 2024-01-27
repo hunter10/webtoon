61a41c03eff6974e07455ff963aa03d189279678
DESKTOP-TD91EH9\dev1 <hunter10apple@gmail.com>
2024-01-27 12:09:31 +0900

    웹툰 앱 초기세팅작업

---
+++ lib/services/api_service.dart
@@ -0,0 +1,16 @@
+import 'package:http/http.dart' as http;
+
+class ApiServices {
+  final String baseurl = "https://webtoon-crawler.nomadcoders.workers.dev";
+  final String today = "today";
+
+  void getTodaysToons() async {
+    final url = Uri.parse('$baseurl/$today');
+    final response = await http.get(url);
+    if (response.statusCode == 200) {
+      print(response.body);
+      return;
+    }
+    throw Error();
+  }
+}
